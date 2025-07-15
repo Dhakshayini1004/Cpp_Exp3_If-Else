3e2919b1fe55abc37865f2b58ded91606e5f9a16
Dhakshayini1004 <dhakshayiniusha@gmail.com>
2025-07-16 00:45:35 +0530

    Create CoordinateSystem.cpp

---
+++ CoordinateSystem.cpp
@@ -0,0 +1,28 @@
+//PRN : 24070123039  A2
+//Name : Dhakshayini Usha R
+#include<iostream>
+using namespace std;
+
+int main(){
+    float x,y;
+    cout<<"Enter x and y coordinates:";
+    cin>>x;
+    cin>>y;
+   
+    if(x>0 && y>0){
+        cout<<"1st Quadrant"<<endl;
+    }
+    else if(x<0 && y>0){
+        cout<<"2nd Quadrant"<<endl;
+    }
+    else if(x<0 && y<0){
+        cout<<"3rd Quadrant"<<endl;
+    }
+    else if(x>0 && y<0){
+        cout<<"4th Quadrant"<<endl;
+    }
+    else{
+        cout<<"It is Origin"<<endl;
+    }
+    return 0;
+}
